e3b48254bd1bbb34600f399e866ed172e42456e5
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-05-22 23:09:08 +0530

    import java.util.Scanner; class odd {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int n,i,s=0,a[];         n=sc.nextInt();         a= new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(i%2==0)             {                 s=s+a[i];             }         }         System.out.print(s);     } }

---
+++ Sum_of_Even_Positions.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class odd
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n,i,s=0,a[];
+        n=sc.nextInt();
+        a= new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(i%2==0)
+            {
+                s=s+a[i];
+            }
+        }
+        System.out.print(s);
+    }
+}
