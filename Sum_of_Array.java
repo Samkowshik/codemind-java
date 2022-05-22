1c3d67219f31388245f4bcfd6bfc55229760b05c
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-05-22 23:11:44 +0530

    import java.util.Scanner; class odd {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int n,i,s=0,a[];         n=sc.nextInt();         a= new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             s=s+a[i];         }         System.out.print(s);     } }

---
+++ Sum_of_Array.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class odd
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n,i,s=0,a[];
+        n=sc.nextInt();
+        a= new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            s=s+a[i];
+        }
+        System.out.print(s);
+    }
+}
