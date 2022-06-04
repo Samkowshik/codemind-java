0d47bde5578e0299e04051c74cd1ae0c8981e601
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-06-04 12:02:52 +0530

    import java.util.Scanner; class ass {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int i,n,f=0,a[];         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         /*for(i=0;i<n;i++)         {             System.out.println(a[i]);         }*/         for(i=1;i<n;i++)         {             if(a[i-1]>=a[i])             {                 f=1;                 break;             }         }         if(f==0)         {             System.out.print("yes");         }         else         {             System.out.print("no");         }     } }

---
+++ Ascending_array.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class ass
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int i,n,f=0,a[];
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        /*for(i=0;i<n;i++)
+        {
+            System.out.println(a[i]);
+        }*/
+        for(i=1;i<n;i++)
+        {
+            if(a[i-1]>=a[i])
+            {
+                f=1;
+                break;
+            }
+        }
+        if(f==0)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+    }
+}
