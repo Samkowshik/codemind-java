1f91730505a3668e42836728d6de811ac3b7ce8d
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-06-04 20:43:59 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,a[],i,x,y,s=0;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         x=sc.nextInt();         y=sc.nextInt();         for(i=0;i<n;i++)         {             if(a[i]>=x&&a[i]<=y)              s=s+a[i];         }         System.out.print(s);     } }

---
+++ sum_of_elements_between_A_and_B.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class sam
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n,a[],i,x,y,s=0;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        x=sc.nextInt();
+        y=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(a[i]>=x&&a[i]<=y)
+             s=s+a[i];
+        }
+        System.out.print(s);
+    }
+}
