465f1463272cbccd134323932d28eaefcab56fe6
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-06-08 11:06:14 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,a[],i,e[],o[],ec=0,oc=0;         n=sc.nextInt();         a=new int[n];         e=new int[n];         o=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             if(a[i]%2==0)             {                 e[ec]=a[i];                 ec++;             }             else             {                 o[oc]=a[i];                 oc++;             }         }         for(i=0;i<oc;i++)         {             System.out.print(o[i]+" ");         }         for(i=0;i<ec;i++)         {             System.out.print(e[i]+" ");         }     } }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class sam
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n,a[],i,e[],o[],ec=0,oc=0;
+        n=sc.nextInt();
+        a=new int[n];
+        e=new int[n];
+        o=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]%2==0)
+            {
+                e[ec]=a[i];
+                ec++;
+            }
+            else
+            {
+                o[oc]=a[i];
+                oc++;
+            }
+        }
+        for(i=0;i<oc;i++)
+        {
+            System.out.print(o[i]+" ");
+        }
+        for(i=0;i<ec;i++)
+        {
+            System.out.print(e[i]+" ");
+        }
+    }
+}
