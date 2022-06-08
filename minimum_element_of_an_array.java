287a0757e317cb2e690aad4e2f790ab26009a115
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-06-08 12:52:03 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,a[],i,m=99999;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             m=a[i]<m?a[i]:m;         }         System.out.print(m);     } }

---
+++ minimum_element_of_an_array.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class sam
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n,a[],i,m=99999;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            m=a[i]<m?a[i]:m;
+        }
+        System.out.print(m);
+    }
+}
