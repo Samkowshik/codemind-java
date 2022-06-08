4bb94921eb992c898fe3b457fa6f586ec75939ef
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-06-08 12:20:06 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,a[],i,c=0;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=1;i<n-1;i++)         {             if(a[i-1]%2!=0 && a[i+1]%2!=0)             {                 if(a[i]%2!=0)                 c++;             }         }         System.out.print(c);     } }

---
+++ count_the_add_numbers_inbetween_odd_numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class sam
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n,a[],i,c=0;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=1;i<n-1;i++)
+        {
+            if(a[i-1]%2!=0 && a[i+1]%2!=0)
+            {
+                if(a[i]%2!=0)
+                c++;
+            }
+        }
+        System.out.print(c);
+    }
+}
