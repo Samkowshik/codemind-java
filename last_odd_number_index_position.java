29709c990f474c52935280ec74024900318f4108
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-06-08 12:30:06 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,a[],i,p=0;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             if(a[i]%2!=0)             p=i;         }         System.out.print(p);     } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class sam
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n,a[],i,p=0;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]%2!=0)
+            p=i;
+        }
+        System.out.print(p);
+    }
+}
