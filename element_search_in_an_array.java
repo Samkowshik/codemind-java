6b4784208a3b446880596eaf72de2772718328cb
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-06-08 12:35:54 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,a[],i,f=0,k;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         k=sc.nextInt();         for(i=0;i<n;i++)         {             if(a[i]==k)             {                 f=1;                 break;             }         }         if(f==1)         System.out.print("True");         else         System.out.print("False");     } }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class sam
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n,a[],i,f=0,k;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==k)
+            {
+                f=1;
+                break;
+            }
+        }
+        if(f==1)
+        System.out.print("True");
+        else
+        System.out.print("False");
+    }
+}
