9b96400b664e5943dcbdedae6bb153ff8be37e74
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-07-14 21:51:41 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         String s1;         Scanner sc= new Scanner(System.in);         s1=sc.nextLine();         System.out.println("Hello Technicalhub");         System.out.println(s1);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class sam
+{
+    public static void main(String args[])
+    {
+        String s1;
+        Scanner sc= new Scanner(System.in);
+        s1=sc.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(s1);
+    }
+}
