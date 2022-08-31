cc296986139cad1906295b04c8df2acd8b74b3e9
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-08-31 12:54:42 +0530

    import java.util.Scanner; class aryele { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int n,a[],i,c=0,c1=0; 		n=sc.nextInt(); 		a=new int[n]; 		for(i=0;i<n;i++) 		{ 			a[i]=sc.nextInt(); 		} 		for(i=0;i<n;i++) 		{ 		    if(a[i]%2==0) 			{ 				c++; 			} 			if(a[i]%2==0&&i%2==0) 			{ 				c1++; 			} 		} 		if(c==c1) 		System.out.print("True"); 		else 		System.out.print("False"); 	} }

---
+++ Strictly_EVEN.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class aryele
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,a[],i,c=0,c1=0;
+		n=sc.nextInt();
+		a=new int[n];
+		for(i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+		    if(a[i]%2==0)
+			{
+				c++;
+			}
+			if(a[i]%2==0&&i%2==0)
+			{
+				c1++;
+			}
+		}
+		if(c==c1)
+		System.out.print("True");
+		else
+		System.out.print("False");
+	}
+}
