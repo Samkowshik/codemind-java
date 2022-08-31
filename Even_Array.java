7b93de49f7d4b65beb097f5cb1f85a66ae8f9b67
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-08-31 12:50:18 +0530

    import java.util.Scanner; class aryele { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int n,a[],i,c=0; 		n=sc.nextInt(); 		a=new int[n]; 		for(i=0;i<n;i++) 		{ 			a[i]=sc.nextInt(); 		} 		for(i=0;i<n;i++) 		{ 			if(a[i]%2==0) 			{ 				c++; 			} 		} 		if(c==n) 		System.out.print("True"); 		else 		System.out.print("False"); 	} }

--- Even_Array.java
+++ Even_Array.java
@@ -1,27 +1,26 @@
 import java.util.Scanner;
-class sam
+class aryele
 {
-    public static void main(String args[])
-    {
-        Scanner sc= new Scanner(System.in);
-        int n,a[],i,c=0;
-        n=sc.nextInt();
-        a=new int[n];
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
-            if(a[i]%2==0)
-            {
-                c++;
-            }
-        }
-        if(c==n)
-        {
-            System.out.print("True");
-        }
-        else
-        {
-            System.out.print("False");
-        }
-    }
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,a[],i,c=0;
+		n=sc.nextInt();
+		a=new int[n];
+		for(i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			if(a[i]%2==0)
+			{
+				c++;
+			}
+		}
+		if(c==n)
+		System.out.print("True");
+		else
+		System.out.print("False");
+	}
 }
