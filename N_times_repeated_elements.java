4bf2e27ab6470838533e429412216b165841280f
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-08-31 13:43:01 +0530

    import java.util.Scanner; class aryele { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int n,a[],i,j,c,k,r=0; 		n=sc.nextInt(); 		a=new int[n]; 		for(i=0;i<n;i++) 		{ 			a[i]=sc.nextInt(); 		} 		k=sc.nextInt(); 		for(i=0;i<n;i++) 		{ 		    if(a[i]==-1) 		    continue; 		    c=1; 		    for(j=0;j<n;j++) 		    { 		        if(i==j) 		        continue; 		        if(a[i]==a[j]) 		        { 		            c++; 		            a[j]=-1; 		        } 		    } 		    if(c==k) 		    {                System.out.print(a[i]+" ");                r=1; 		    } 		} 		if(r==0) 		System.out.print("-1"); 	} }

--- N_times_repeated_elements.java
+++ N_times_repeated_elements.java
@@ -1,37 +1,39 @@
 import java.util.Scanner;
-class sam
+class aryele
 {
-    public static void main(String args[])
-    {
-        Scanner sc= new Scanner(System.in);
-        int n,a[],i,j,c,f=0,k;
-        n=sc.nextInt();
-        a= new int[n];
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
-        }
-        k=sc.nextInt();
-        for(i=0;i<n;i++)
-        {
-            c=1;
-            if(a[i]==-1)
-            continue;
-            for(j=0;j<n;j++)
-            {
-                if(a[i]==a[j] && i!=j)
-                {
-                    a[j]=-1;
-                    c++;
-                }
-            }
-            if(k==c)
-            {
-                f=1;
-                System.out.print(a[i]+" ");
-            }
-        }
-        if(f==0)
-        System.out.print("-1");
-    }
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,a[],i,j,c,k,r=0;
+		n=sc.nextInt();
+		a=new int[n];
+		for(i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+		k=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+		    if(a[i]==-1)
+		    continue;
+		    c=1;
+		    for(j=0;j<n;j++)
+		    {
+		        if(i==j)
+		        continue;
+		        if(a[i]==a[j])
+		        {
+		            c++;
+		            a[j]=-1;
+		        }
+		    }
+		    if(c==k)
+		    {
+               System.out.print(a[i]+" ");
+               r=1;
+		    }
+		}
+		if(r==0)
+		System.out.print("-1");
+	}
 }
