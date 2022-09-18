fbb9baa11b5801606b48efd8d7bf6dc427c3754c
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-09-18 19:20:19 +0530

    import java.util.Scanner; class sam { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int n,a[],c=0,f,s=0,j,i; 		n=sc.nextInt(); 		a=new int[n]; 		for(i=0;i<n;i++) 		{ 			a[i]=sc.nextInt(); 		} 		for(i=0;i<n;i++) 		{ 		    f=0; 		    if(a[i]==-1) 			continue; 			for(j=0;j<n;j++) 			{ 			    if(a[i]==a[j] && i!=j) 			    { 			        a[j]=-1; 			    } 			} 			s=s+a[i]; 		} 		System.out.print(s); 	} }

---
+++ unique_sum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class sam
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,a[],c=0,f,s=0,j,i;
+		n=sc.nextInt();
+		a=new int[n];
+		for(i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+		    f=0;
+		    if(a[i]==-1)
+			continue;
+			for(j=0;j<n;j++)
+			{
+			    if(a[i]==a[j] && i!=j)
+			    {
+			        a[j]=-1;
+			    }
+			}
+			s=s+a[i];
+		}
+		System.out.print(s);
+	}
+}
