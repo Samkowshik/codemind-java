56b164ed816f54053beff49db51842180e914064
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-09-18 21:56:57 +0530

    import java.util.Scanner; class sam { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int n,a[],c=0,i,r,d,s; 		n=sc.nextInt(); 		a=new int[n]; 		for(i=0;i<n;i++) 		{ 			a[i]=sc.nextInt(); 		} 		for(i=0;i<n;i++) 		{ 		    d=a[i]; 		    s=0; 			while(d!=0) 			{ 			    r=d%10; 			    s=s*10+r; 			    d=d/10; 			} 			if(s==a[i]) 			c++; 		} 		System.out.print(c); 	} }

--- Count_palindromes.java
+++ Count_palindromes.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
-class pal
+class sam
 {
-    public static void main(String args[])
-    {
-        Scanner sc= new Scanner(System.in);
-        int n,i,a[],r,s,d,c=0;
-        n=sc.nextInt();
-        a= new int[n];
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
-        }
-        for(i=0;i<n;i++)
-        {
-            d=a[i];
-            s=0;
-            while(d>0)
-            {
-                r=d%10;
-                s=s*10+r;
-                d=d/10;
-            }
-            if(s==a[i])
-            c++;
-        }
-        System.out.println(c);
-    }
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,a[],c=0,i,r,d,s;
+		n=sc.nextInt();
+		a=new int[n];
+		for(i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+		    d=a[i];
+		    s=0;
+			while(d!=0)
+			{
+			    r=d%10;
+			    s=s*10+r;
+			    d=d/10;
+			}
+			if(s==a[i])
+			c++;
+		}
+		System.out.print(c);
+	}
 }
