123afabcbfd7eaf874847fe1afa84dcba14be104
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-09-18 21:37:48 +0530

    import java.util.Scanner; class sam { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int n,a[],c=0,i; 		n=sc.nextInt(); 		a=new int[n]; 		for(i=0;i<n;i++) 		{ 			a[i]=sc.nextInt(); 		} 		for(i=0;i<n-1;i++) 		{ 			if(a[i]<a[i+1]) 			c++; 		} 		if(c==n-1) 		System.out.print("yes"); 		else 		System.out.print("no"); 	} }

--- Ascending_array.java
+++ Ascending_array.java
@@ -1,35 +1,24 @@
 import java.util.Scanner;
-class ass
+class sam
 {
-    public static void main(String args[])
-    {
-        Scanner sc= new Scanner(System.in);
-        int i,n,f=0,a[];
-        n=sc.nextInt();
-        a=new int[n];
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
-        }
-        /*for(i=0;i<n;i++)
-        {
-            System.out.println(a[i]);
-        }*/
-        for(i=1;i<n;i++)
-        {
-            if(a[i-1]>=a[i])
-            {
-                f=1;
-                break;
-            }
-        }
-        if(f==0)
-        {
-            System.out.print("yes");
-        }
-        else
-        {
-            System.out.print("no");
-        }
-    }
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,a[],c=0,i;
+		n=sc.nextInt();
+		a=new int[n];
+		for(i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n-1;i++)
+		{	
+			if(a[i]<a[i+1])
+			c++;
+		}
+		if(c==n-1)
+		System.out.print("yes");
+		else
+		System.out.print("no");
+	}
 }
