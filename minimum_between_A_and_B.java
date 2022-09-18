dda6f91fb9de50132340b3016236c254f0046077
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-09-18 20:41:14 +0530

    import java.util.Scanner; class sam { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int n,a[],c=0,x,b,j,i,f=1,m=99999; 		n=sc.nextInt(); 		a=new int[n]; 		for(i=0;i<n;i++) 		{ 			a[i]=sc.nextInt(); 		} 		x=sc.nextInt(); 		b=sc.nextInt(); 		for(i=0;i<n;i++) 		{ 		    for(j=x;j<=b;j++) 		    { 		        if(j==a[i]) 		        { 		            m=a[i]<m?a[i]:m; 		            f=0; 		        } 		    } 		} 		if(f==1) 		System.out.print("-1"); 		else 		System.out.print(m); 	} }

--- minimum_between_A_and_B.java
+++ minimum_between_A_and_B.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
 class sam
 {
-    public static void main(String args[])
-    {
-        Scanner sc= new Scanner(System.in);
-        int n,a[],i,x,y,f=0,m=99999;
-        n=sc.nextInt();
-        a=new int[n];
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
-        }
-        x=sc.nextInt();
-        y=sc.nextInt();
-        for(i=0;i<n;i++)
-        {
-            if(a[i]>=x && a[i]<=y)
-            {
-                f=1;
-                m=a[i]<m?a[i]:m;
-            }
-        }
-        if(f==0)
-        System.out.print("-1");
-        else
-        System.out.print(m);
-    }
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,a[],c=0,x,b,j,i,f=1,m=99999;
+		n=sc.nextInt();
+		a=new int[n];
+		for(i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+		x=sc.nextInt();
+		b=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+		    for(j=x;j<=b;j++)
+		    {
+		        if(j==a[i])
+		        {
+		            m=a[i]<m?a[i]:m;
+		            f=0;
+		        }
+		    }
+		}
+		if(f==1)
+		System.out.print("-1");
+		else
+		System.out.print(m);
+	}
 }
