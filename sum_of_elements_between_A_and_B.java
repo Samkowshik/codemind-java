03e5f1c19fce1ffc6c4c763e4bc9c4e83ec0b2a5
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-09-18 20:43:13 +0530

    import java.util.Scanner; class sam { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int n,a[],c=0,x,b,j,i,f=1,s=0; 		n=sc.nextInt(); 		a=new int[n]; 		for(i=0;i<n;i++) 		{ 			a[i]=sc.nextInt(); 		} 		x=sc.nextInt(); 		b=sc.nextInt(); 		for(i=0;i<n;i++) 		{ 		    for(j=x;j<=b;j++) 		    { 		        if(j==a[i]) 		        { 		            s=s+a[i]; 		            f=0; 		        } 		    } 		} 		if(f==1) 		System.out.print("-1"); 		else 		System.out.print(s); 	} }

--- sum_of_elements_between_A_and_B.java
+++ sum_of_elements_between_A_and_B.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
 class sam
 {
-    public static void main(String args[])
-    {
-        Scanner sc= new Scanner(System.in);
-        int n,a[],i,x,y,s=0;
-        n=sc.nextInt();
-        a=new int[n];
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
-        }
-        x=sc.nextInt();
-        y=sc.nextInt();
-        for(i=0;i<n;i++)
-        {
-            if(a[i]>=x&&a[i]<=y)
-             s=s+a[i];
-        }
-        System.out.print(s);
-    }
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,a[],c=0,x,b,j,i,f=1,s=0;
+		n=sc.nextInt();
+		a=new int[n];
+		for(i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+		x=sc.nextInt();
+		b=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+		    for(j=x;j<=b;j++)
+		    {
+		        if(j==a[i])
+		        {
+		            s=s+a[i];
+		            f=0;
+		        }
+		    }
+		}
+		if(f==1)
+		System.out.print("-1");
+		else
+		System.out.print(s);
+	}
 }
