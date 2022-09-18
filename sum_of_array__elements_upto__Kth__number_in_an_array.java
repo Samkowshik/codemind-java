901bb1f84f7106281ed0bcb914f6b84d8168e224
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-09-18 20:54:53 +0530

    import java.util.Scanner; class sam { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int n,a[],k,s=0,i; 		n=sc.nextInt(); 		a=new int[n]; 		for(i=0;i<n;i++) 		{ 			a[i]=sc.nextInt(); 		} 		k=sc.nextInt(); 		for(i=0;i<k;i++) 		{ 			s=s+a[i]; 		} 		System.out.print(s); 	} }

--- sum_of_array__elements_upto__Kth__number_in_an_array.java
+++ sum_of_array__elements_upto__Kth__number_in_an_array.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 class sam
 {
-    public static void main(String args[])
-    {
-        Scanner sc= new Scanner(System.in);
-        int n,a[],i,k,s=0;
-        n=sc.nextInt();
-        a=new int[n];
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
-        }
-        k=sc.nextInt();
-        for(i=0;i<k;i++)
-        {
-            s=s+a[i];
-        }
-        System.out.print(s);
-    }
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,a[],k,s=0,i;
+		n=sc.nextInt();
+		a=new int[n];
+		for(i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+		k=sc.nextInt();
+		for(i=0;i<k;i++)
+		{	
+			s=s+a[i];
+		}
+		System.out.print(s);
+	}
 }
