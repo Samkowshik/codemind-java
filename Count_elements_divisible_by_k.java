cbf1696e93162c9be163673399d82c3e6f80cd16
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-09-18 21:50:40 +0530

    import java.util.Scanner; class sam { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int n,a[],k,c=0,i; 		n=sc.nextInt(); 		k=sc.nextInt(); 		a=new int[n]; 		for(i=0;i<n;i++) 		{ 			a[i]=sc.nextInt(); 		} 		for(i=0;i<n;i++) 		{ 			if(a[i]%k==0) 			c++; 		} 		System.out.print(c); 	} }

--- Count_elements_divisible_by_k.java
+++ Count_elements_divisible_by_k.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
-class a
+class sam
 {
-    public static void main(String args[])
-    {
-        int n,a[],i,k,c=0;
-        Scanner sc= new Scanner(System.in);
-        n= sc.nextInt();
-        k=sc.nextInt();
-        a=new int[n];
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
-            if(a[i]%k==0)
-            c++;
-        }
-        System.out.println(c);
-    }
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,a[],k,c=0,i;
+		n=sc.nextInt();
+		k=sc.nextInt();
+		a=new int[n];
+		for(i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{	
+			if(a[i]%k==0)
+			c++;
+		}
+		System.out.print(c);
+	}
 }
