348edcf1bf2c21a1f85d50f437b0ea286bbecaee
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-09-21 20:55:51 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,a[],i,x,k,c=0;         n=sc.nextInt();         k=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             if(a[i]==0)             a[i]=1;             if(a[i]<0)             a[i]=a[i]*-1;         }         for(i=0;i<n;i++)         {             x=(int)Math.log10(a[i])+1;             if(x==k)             c++;         }         System.out.println(c);     } }

---
+++ K_digit_length_numbers_in_a_array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class sam
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n,a[],i,x,k,c=0;
+        n=sc.nextInt();
+        k=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]==0)
+            a[i]=1;
+            if(a[i]<0)
+            a[i]=a[i]*-1;
+        }
+        for(i=0;i<n;i++)
+        {
+            x=(int)Math.log10(a[i])+1;
+            if(x==k)
+            c++;
+        }
+        System.out.println(c);
+    }
+}
