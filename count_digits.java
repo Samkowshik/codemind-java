1adf4fd54651592e25df97ff17b3efca8b05bce6
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-09-21 20:59:36 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,a[],i,x;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             if(a[i]==0)             a[i]=1;             if(a[i]<0)             a[i]=a[i]*-1;         }         for(i=0;i<n;i++)         {             x=(int)Math.log10(a[i])+1;             System.out.print(x+" ");         }     } }

---
+++ count_digits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class sam
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n,a[],i,x;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]==0)
+            a[i]=1;
+            if(a[i]<0)
+            a[i]=a[i]*-1;
+        }
+        for(i=0;i<n;i++)
+        {
+            x=(int)Math.log10(a[i])+1;
+            System.out.print(x+" ");
+        }
+    }
+}
