f78cf82dc7f41bfc512f7d32ae1bc6cad7858ef7
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-09-28 15:14:32 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,d,s=0,r;         n=sc.nextInt();         d=n;         if(n<0)         n=n*-1;         while(n%10==0)         {             n=n/10;         }         while(n!=0)         {             r=n%10;             s=s*10+r;             n=n/10;         }         if(d>0)         System.out.print(s);         else         System.out.print(s*-1);     } }

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 class sam
 {
-    public static void main (String args[])
+    public static void main(String args[])
     {
         Scanner sc= new Scanner(System.in);
-        int n,i,d,r,s=0;
+        int n,d,s=0,r;
         n=sc.nextInt();
         d=n;
-        if(d<0)
-        d=d*-1;
-        while(d%10==0)
-        {
-            d=d/10;
-        }
-        while(d!=0)
-        {
-            r=d%10;
-            s=s*10+r;
-            d=d/10;
-        }
         if(n<0)
-        System.out.println(s*-1);
+        n=n*-1;
+        while(n%10==0)
+        {
+            n=n/10;
+        }
+        while(n!=0)
+        {
+            r=n%10;
+            s=s*10+r;
+            n=n/10;
+        }
+        if(d>0)
+        System.out.print(s);
         else
-        System.out.println(s);
+        System.out.print(s*-1);
     }
 }
