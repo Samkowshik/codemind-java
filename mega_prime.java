21f96dec3507088d5a34298b10de4199951daa9b
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-09-29 14:52:16 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,r,d,i,f=0,c1=0,c=0;         n=sc.nextInt();         for(i=2;i<n;i++)         {             if(n%i==0)             {                 f=1;                 break;             }         }         while(n>0)         {             r=n%10;             if(r==2||r==3||r==5||r==7)             {                 c1++;             }             n=n/10;             c++;         }         if(f==0&&(c==c1))         System.out.print("Mega Prime");         else         System.out.print("Not Mega Prime");     } }

--- mega_prime.java
+++ mega_prime.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 class sam
 {
-    public static void main (String args[])
+    public static void main(String args[])
     {
         Scanner sc= new Scanner(System.in);
-        int n,i,x,f=0,c=0,r;
+        int n,r,d,i,f=0,c1=0,c=0;
         n=sc.nextInt();
-        x=(int)Math.log10(n)+1;
         for(i=2;i<n;i++)
         {
             if(n%i==0)
@@ -15,21 +14,19 @@ class sam
                 break;
             }
         }
-        if(f==0)
+        while(n>0)
         {
-            while(n!=0)
+            r=n%10;
+            if(r==2||r==3||r==5||r==7)
             {
-                r=n%10;
-                if(r==2||r==3||r==5||r==7)
-                c++;
-                n=n/10;
+                c1++;
             }
-            if(c==x)
-            System.out.println("Mega Prime");
-            else
-            System.out.println("Not Mega Prime");
+            n=n/10;
+            c++;
         }
+        if(f==0&&(c==c1))
+        System.out.print("Mega Prime");
         else
-        System.out.println("Not Mega Prime");
+        System.out.print("Not Mega Prime");
     }
 }
