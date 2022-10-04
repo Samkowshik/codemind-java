935f7eb4d2c02528edaea02ff8e279ac976e6edc
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-10-04 18:14:33 +0530

    import java.util.Scanner; class sam {     public static void main (String args[])     {         Scanner sc = new Scanner(System.in);         int n,i,j,a[],f,c=0;         n=sc.nextInt();         a= new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(a[i]==1)             continue;             f=0;             for(j=2;j<a[i];j++)             {                 if(a[i]%j==0)                 {                     f=1;                     break;                 }             }             if(f==0)             c++;         }         System.out.println(c);     } }

--- Primes_in_the_array.java
+++ Primes_in_the_array.java
@@ -3,19 +3,19 @@ class sam
 {
     public static void main (String args[])
     {
-        Scanner sc= new Scanner(System.in);
-        int n,a[],i,f,c=0,j;
+        Scanner sc = new Scanner(System.in);
+        int n,i,j,a[],f,c=0;
         n=sc.nextInt();
-        a=new int[n];
+        a= new int[n];
         for(i=0;i<n;i++)
         {
             a[i]=sc.nextInt();
         }
         for(i=0;i<n;i++)
         {
-            f=0;
             if(a[i]==1)
             continue;
+            f=0;
             for(j=2;j<a[i];j++)
             {
                 if(a[i]%j==0)
