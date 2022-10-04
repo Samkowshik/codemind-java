8927a457fd1d12bbd2b072be1cdcdda92e30d39d
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-10-04 20:58:34 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n1,n2,n,i,c=0,f;         n1=sc.nextInt();         n2=sc.nextInt();         for(n=n1;n<=n2;n++)         {             if(n==1)             continue;             f=0;             for(i=2;i<n;i++)             {                 if(n%i==0)                 {                     f=1;                     break;                 }             }             if(f==0)             System.out.println(n);         }     } }

--- Generate_Prime_Numbers_In_A_Interval_.java
+++ Generate_Prime_Numbers_In_A_Interval_.java
@@ -4,23 +4,23 @@ class sam
     public static void main(String args[])
     {
         Scanner sc= new Scanner(System.in);
-        int n1,n2,n,i,c,f;
+        int n1,n2,n,i,c=0,f;
         n1=sc.nextInt();
         n2=sc.nextInt();
         for(n=n1;n<=n2;n++)
         {
-            f=1;
             if(n==1)
             continue;
+            f=0;
             for(i=2;i<n;i++)
             {
                 if(n%i==0)
                 {
-                    f=0;
+                    f=1;
                     break;
                 }
             }
-            if(f==1)
+            if(f==0)
             System.out.println(n);
         }
     }
