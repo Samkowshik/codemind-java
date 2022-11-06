e27b6a214ec545eba47fd0e49734c3d62c5d4614
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-11-06 21:46:46 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,i,j;         n=sc.nextInt();         for(i=1;i<=n;i++)         {             for(j=1;j<=n;j++)             {                 if  (i==j ||j==1||j==n)                 System.out.print("* ");                 else                 System.out.print("  ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -4,23 +4,18 @@ class sam
     public static void main(String args[])
     {
         Scanner sc= new Scanner(System.in);
-        int n,i,j,a[];
+        int n,i,j;
         n=sc.nextInt();
-        a=new int[n];
-        for(i=1;i<n;i++)
+        for(i=1;i<=n;i++)
         {
             for(j=1;j<=n;j++)
             {
-                if(i==j || j==1)
-                System.out.print("*");
+                if  (i==j ||j==1||j==n)
+                System.out.print("* ");
                 else
-                System.out.print(" ");
+                System.out.print("  ");
             }
             System.out.println();
-        }
-        for(i=1;i<=n;i++)
-        {
-            System.out.print("*");
         }
     }
 }
