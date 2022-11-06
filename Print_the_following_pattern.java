3d1aa5dab5a9e30156628f943550264247b239f2
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-11-06 21:55:08 +0530

    import java.util.Scanner; class sam {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n,i,j;         n=sc.nextInt();         for(i=n;i>0;i--)         {             for(j=n;j>0;j--)             {                 if(i==j || i+j==n+1)                 System.out.print(i+" ");                 else                 System.out.print(" ");             }             System.out.println();         }     } }

--- Print_the_following_pattern.java
+++ Print_the_following_pattern.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
-class pat
+class sam
 {
-    public static void main(String[] args)
+    public static void main(String args[])
     {
         Scanner sc= new Scanner(System.in);
         int n,i,j;
         n=sc.nextInt();
-        for(i=1;i<=n;i++)
+        for(i=n;i>0;i--)
         {
-            for(j=i;j<=n;j++)
+            for(j=n;j>0;j--)
             {
-                System.out.print(j+" ");
+                if(i==j || i+j==n+1)
+                System.out.print(i+" ");
+                else
+                System.out.print(" ");
             }
             System.out.println();
         }
