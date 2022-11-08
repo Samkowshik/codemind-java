a4bb630fb8485d1f8f7fe985e76b805729330ee3
Samkowshik <96334386+Samkowshik@users.noreply.github.com>
2022-11-08 22:11:03 +0530

    import java.util.Scanner; class sam { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		int n,a[],c=0,f,j,i; 		n=sc.nextInt(); 		a=new int[n]; 		for(i=0;i<n;i++) 		{ 			a[i]=sc.nextInt(); 		} 		for(i=0;i<n;i++) 		{ 		    if(a[i]==-1) 			continue; 			for(j=0;j<n;j++) 			{ 			    if(a[i]==a[j] && i!=j) 			    { 			        a[j]=a[i]=-1; 			        c++; 			        break; 			    } 			} 		} 		System.out.print(c); 	} }

---
+++ CLOTHING_STORE.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class sam
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,a[],c=0,f,j,i;
+		n=sc.nextInt();
+		a=new int[n];
+		for(i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+		    if(a[i]==-1)
+			continue;
+			for(j=0;j<n;j++)
+			{
+			    if(a[i]==a[j] && i!=j)
+			    {
+			        a[j]=a[i]=-1;
+			        c++;
+			        break;
+			    }
+			}
+		}
+		System.out.print(c);
+	}
+}
